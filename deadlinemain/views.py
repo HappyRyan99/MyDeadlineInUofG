bc557aa82f77dfc323af26d8f2f864a5a32d6212
Haoran Liu <3075533L@student.gla.ac.uk>
2026-02-20 21:25:16 +0000

    config index

--- deadlinemain/views.py
+++ deadlinemain/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context_dict = {}
-    return render(request, '', context=context_dict)
+    return render(request, 'login.html')
