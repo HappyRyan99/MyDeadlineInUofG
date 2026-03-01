03fe2eced822f90d4710c0ff6be278b7e522698d
XIN YAN <3103902Y@student.gla.ac.uk>
2026-03-01 10:52:04 +0000

    更新urls

--- deadlinemain/urls.py
+++ deadlinemain/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import register_view, login_view  # 对应你的视图函数
 
 urlpatterns = [
-    path('', views.index, name='index'), # Default to login
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),  # 对应模板里的login链接
 ]
