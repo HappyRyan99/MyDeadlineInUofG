8f57f8481e1d88beeabb6c7ad1d1f04fa3e55c43
XIN YAN <3103902Y@student.gla.ac.uk>
2026-03-02 22:44:57 +0000

    Py documents upload
    # Conflicts:
    #	deadlinemain/urls.py
    #	deadlinemain/views.py

--- deadlinemain/urls.py
+++ deadlinemain/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from . import views
-from .views import register_view, login_view  # 对应你的视图函数
 
 urlpatterns = [
-    path('courses/', views.course_list, name='course_list'),
-    path('groups/', views.my_groups, name='my_groups'),
-    path('add_course/', views.add_course, name='add_course'),
-    path('delete_course/', views.delete_course, name='delete_course'),
-    path('edit_course/', views.edit_course, name='edit_course'),
-
-    path("register/", register_view, name="register"),
-    path("login/", login_view, name="login"),  # 对应模板里的login链接
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('', views.home_view, name='home'),
 ]
