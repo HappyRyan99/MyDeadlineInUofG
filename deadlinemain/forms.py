599f17658c66938a1c5e49b9919538249302465d
Haoran Liu <3075533L@student.gla.ac.uk>
2026-03-03 21:33:19 +0000

    reg user bug fix. havent add to student table

--- deadlinemain/forms.py
+++ deadlinemain/forms.py
@@ -2,20 +2,29 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
+    student_id = forms.CharField(label=("Student Id"),
+                                 widget=forms.TextInput(attrs={'class': 'form-control'})
+                                 )
+
     email = forms.EmailField(
         required=True,
         widget=forms.EmailInput(attrs={'class': 'form-control'})
     )
+    password1 = forms.CharField(label=("Password"),
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'})
+                                )
+    password2 = forms.CharField(label=("Password Confirmation"),
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'})
+                                )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-        }
+        fields = ('student_id', 'email')
+
 
 class LoginForm(forms.Form):
     student_id = forms.CharField(
