c5a96b3bef5fe8b7d2ebe015008dab3dc4b29061
Haoran Liu <3075533L@student.gla.ac.uk>
2026-03-03 22:41:02 +0000

    default 404 page

--- UofGdeadline/settings.py
+++ UofGdeadline/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-!xn7i-!nif^#67340+egn-gi&m19+hf*(d2k6+d_2r_nld6ad9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
