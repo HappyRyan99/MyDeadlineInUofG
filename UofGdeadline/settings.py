410effc31ba71cad112f206f5e0d5c78ca71adc3
Haoran Liu <3075533L@student.gla.ac.uk>
2026-03-05 15:44:06 +0000

    release Tag

--- UofGdeadline/settings.py
+++ UofGdeadline/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!xn7i-!nif^#67340+egn-gi&m19+hf*(d2k6+d_2r_nld6ad9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
 
